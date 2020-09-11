bfdb679acc6b902b51bf35d29fca6135d9b5e451
Johan794 <johan.ricardo1104@gmail.com>
2020-09-10 22:04:55 -0500

    Version 1

---
+++ src/Proyecto_integrador1.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class Proyecto_integrador1{
+	enum kind{obra_negra,obra_blanca};
+	enum Location{norte,centro,sur};
+	public static final int BLACK_WORK=1300000;
+	public static final int WHITE_WORK= 2600000;
+	public static final int PAINTING= 980000;
+	
+	public static void main(String []args){
+		Scanner sc= new Scanner(System.in);
+		int quantity;
+		int materials_list[];
+		int quantity_materials[];
+		
+		System.out.println("Please type the amount of materials needed in the construction");
+         quantity= sc.nextInt();		
+	     materials_list= new int[quantity];
+		 quantity_materials= new int[quantity];
+		
+		 System.out.println("Please type the materials needed in the construction");
+		for(int i=0; i<materials_list.length; i++){
+			System.out.println("Please type the material # "+(i+1) );
+			    materials_list[i]= sc.nextInt();
+		}
+		
+		
+	   System.out.println("Please type the amount of materials needed in the construction");                      
+		for(int j=0; j<quantity_materials.length; j++){
+		      System.out.println("Please type the amount of the material # "+ (j+1) );
+			    quantity_materials[j]= sc.nextInt();
+		
+		}
+	
+	
+		
+		
+		
+		
+		
+		
+		
+	
+	}
+	
+	
+	//ver la utilidad de enum para declarar el resto de metodos
+	/*public static int[] materials_list(int array[] ){
+	  
+	
+
+	
+	}*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
