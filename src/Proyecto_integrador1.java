ab5b679c08558827e701d61238202064e143dc42
Johan794 <johan.ricardo1104@gmail.com>
2020-09-19 19:54:55 -0500

    Version final entrega 1

--- src/Proyecto_integrador1.java
+++ src/Proyecto_integrador1.java
@@ -49,7 +49,7 @@ public class Proyecto_integrador1{
 			System.out.println("please type the price of "+materials_list[n]+" in the downtown store");
 			downtown_store[n]= sc.nextInt();
 		
-			System.out.println("please type the price of "+materials_list[n]+" the in neighborhood store");
+			System.out.println("please type the price of "+materials_list[n]+"  in the neighborhood store");
 			neighborhood_store[n]= sc.nextInt();
 		
 		}
@@ -57,10 +57,10 @@ public class Proyecto_integrador1{
 		total_topay=howmuctopayin(kind,home_center,downtown_store,neighborhood_store,quantity_materials);
 	    System.out.println("The price of the construction if you decide to buy in Home center is "+total_topay[0]+" plus workforce");
 		System.out.println("The price of the construction if you decide to buy in Downtown Store is "+total_topay[1]+" plus workforce");
-		System.out.println("The price of the construction if you decide to buy in the neigborhood store is "+total_topay[2]+"plus workforce");
-		
-		bestplace_tobuy(home_center, downtown_store, neighborhood_store, quantity, materials_list);
-		type_ofuse(black_work,white_work,painting,kind,materials_list);	
+		System.out.println("The price of the construction if you decide to buy in the neigborhood store is "+total_topay[2]+" plus workforce");
+		System.out.println();
+		bestplace_tobuy(home_center, downtown_store, neighborhood_store, quantity, materials_list,quantity_materials,kind);
+		type_ofuse(kind,materials_list);	
 		
 		
 		
@@ -72,12 +72,12 @@ public class Proyecto_integrador1{
 	
 	/**
 	* Method: kindofwork:<br>
-	* This method helps to clasficate the use that will be given to f(the_best_buy>=neighborhood_store[i]a Material of the list of materials <br>
+	* This method helps to clasficate the use that will be given to the list of materials <br>
 	* <b> pre: </b> It requires that the list of materials is not empty and the varibale quantity must be diferent of 0 <br>
 	* <b> pos: </b> The method will give an array of objects which will have the kind of work that has been asigned by the user <br>
-	* @param String[] array,  the list of materias
+	* @param array,  the list of materias
 	* @param quantity, the size of the material's list
-	* @return kind[] ,It is an array of objects 
+	* @return kind ,It is an array of objects 
 	*/
 	public static Work[] kindofwork(String []array, int quantity){
 		Work []kind= new Work[quantity];
@@ -103,14 +103,14 @@ public class Proyecto_integrador1{
 	/**
 	* Method: howmuchtopay <br>
 	* This method calculates how much must the user pay for the work if he decides to buy the materials at and espesific store <br>
-	* <b> pre: </b> The prices that the user has given in the main methohd should be diferents of 0. <br>
+	* <b> pre: </b> the enumeration should have been assigned before. <br>
 	* <b> pos: </b> The method will give the complete price of the construction for each store, like he would buy at one of those stores <br>
-	* @param []work, the array with the kind of work 
-	* @param  []home_center, the array with the prices of Home center
-	* @param  []downtown_store, the array with the prices of the downtown store
-	* @param []neighborhood_store, the array with the prices of the Neighborhood store
-	* @param [] materials, the array that contains the amount of materias
-	* @return total_topay[], it is an array of integers it includes the price of hand working
+	* @param work, the array with the kind of work 
+	* @param  home_center, the array with the prices of Home center
+	* @param downtown_store, the array with the prices of the downtown store
+	* @param neighborhood_store, the array with the prices of the Neighborhood store
+	* @param  materials, the array that contains the amount of materias
+	* @return total_topay, it is an array of integers it includes the price of hand working
 	*/
 	public static int[] howmuctopayin(Work[] work, int []home_center, int []downtown_store, int []neighborhood_store, double[] materials){
 		int pay_homecenter=0;
@@ -169,23 +169,29 @@ public class Proyecto_integrador1{
 	}
 	/**
 	* Method: bestplace_tobuy <br>
-	* this method will show the best place to buy each material of the list, and will caulcute the budget if the user decides to show those suggestions <br> 
-	* <b> pre: </b> <br>
-   	* <b> post: the method will show the places with the lower prices of each material and a better budget in that case<br>
-	* @param home_center[] the prices of the materials in Home Center
-	* @param dowtown_store[] the prices of the materials in
-	* @param neigborhood_store[] the prices of the materials in
-	* @param quantity the amount of materils to save the best prices in another list
-	* @param array[]  it contains the list of materials
+	* this method will show the best place to buy each material of the list, and will caulcute the budget if the user decides to show those suggestions it includes delivery <br> 
+	* <b> pre: </b> the user must indicate the location for the delivery<br>
+   	* <b> post: </b> the method will show the places with the lower prices of each material and a better budget in that case, it includes delivery and workforce<br>
+	* @param home_center the prices of the materials in Home Center
+	* @param dowtown_store the prices of the materials in the downtown store.
+	* @param neigborhood_store the prices of the materials in the neigborhood store.
+	* @param quantity the size of the list of materials to save the best prices in another list
+	* @param array  it contains the list of materials
+	* @param materials  the amound of materials
+	* @param kind the type of of use for each material
 	* @return 
 	*/
-	
-	public static void bestplace_tobuy(int []home_center, int []downtown_store, int []neighborhood_store, int quantity, String array[]){
+	public static void bestplace_tobuy(int []home_center, int []downtown_store, int []neighborhood_store, int quantity, String array[], double []materials, Work kind[]){
 		int the_best_buy;
 		int final_bestbudget=0;
 		String buy="";
 		String best_election[]= new String[quantity];
 		int best_budget[]= new int[quantity];
+		int var;
+		boolean black_work=false;
+		boolean white_work=false;
+		boolean painting_work=false;
+		Location delivery=null;
 		for(int i=0; i<best_election.length; i++){
 			the_best_buy=home_center[i];
 			if((the_best_buy>=downtown_store[i])&&(the_best_buy>=neighborhood_store[i])){
@@ -195,7 +201,6 @@ public class Proyecto_integrador1{
 				    the_best_buy=neighborhood_store[i];
 					buy="Neighborhood Store";
 				      }
-			
 			}else if(the_best_buy>=downtown_store[i]){
 	                   the_best_buy=neighborhood_store[i];			
 				       buy="Down town Store";
@@ -208,34 +213,101 @@ public class Proyecto_integrador1{
 			                          }
 		      best_election[i]=buy;
 			  best_budget[i]=the_best_buy;
-		}
+		}	
 	 for(int j=0; j<best_election.length; j++){
 		 System.out.println("The best place to buy "+array[j]+" is "+best_election[j]);
-		 final_bestbudget+= best_budget[j];
-		 
+		 final_bestbudget+=(best_budget[j]*materials[j]); 
 	 }
-         System.out.println("In that case the price of the construction would be "+final_bestbudget);	 	
-	
+		
+        for(int k=0; k<best_election.length; k++){
+			if(kind[k]==Work.OBRA_NEGRA){   black_work=true; }
+			if(kind[k]== Work.OBRA_BLANCA){  white_work=true;   }
+			if(kind[k]== Work.PINTURA){  painting_work=true;  }
+		}
+		if(((black_work && white_work) && painting_work)==true){
+				final_bestbudget+=(BLACK_WORK+WHITE_WORK+PAINTING);
+				
+				}else if((black_work && white_work)==true){
+				        final_bestbudget+=(BLACK_WORK+WHITE_WORK);
+				       
+			
+			    }else if((black_work && painting_work)==true){
+				        final_bestbudget+=(BLACK_WORK+PAINTING);
+				        
+			
+			    }else if((white_work && painting_work)==true){
+				         final_bestbudget+=(WHITE_WORK+PAINTING);
+			        }
+		
+		System.out.println("So the price of the construction would be "+final_bestbudget+" whitout delivery");
+		 System.out.println("Where is the building zone?\n 1: North \n 2: DownTown \n 3: South ");
+          var=sc.nextInt();
+		  switch(var){
+			  case 1: delivery= Location.NORTE;
+				  break;
+			  case 2: delivery= Location.CENTRO;
+				  break;
+			  case 3: delivery= Location.SUR;
+				  
+		  }
+		if(delivery==Location.NORTE){
+			if(final_bestbudget<80000){
+				final_bestbudget+=120000;
+			}else if(final_bestbudget<300000){
+				final_bestbudget+=28000;
+			}else if(final_bestbudget>=300000){
+				final_bestbudget+=0;
+				var=0;
+			}
+		}else if(delivery== Location.CENTRO){
+			if(final_bestbudget<80000){
+				final_bestbudget+=50000;
+			}else if(final_bestbudget<300000){
+				final_bestbudget+=0;
+				var=0;
+			}else if(final_bestbudget>=300000){
+				final_bestbudget+=0;
+				var=0;
+			}
+		}else if(delivery == Location.SUR){
+			if(final_bestbudget<80000){
+				final_bestbudget+=120000;
+			}else if(final_bestbudget<300000){
+				final_bestbudget+=55000;
+			}else if(final_bestbudget>=300000){
+				final_bestbudget+=0;
+				var=0;
+			}
+		}
+		if(var==0){
+           System.out.println("In that case the price of the construction would be "+final_bestbudget+" delivery free");
+		}else{
+		   System.out.println("In that case the price of the construction would be "+final_bestbudget+" plus delivery");
+		}
+		
+		
+		
+		
+		
+		
 	}
 	
 	/**
 	* Method: type_ofuse <br>
 	*  this method will show all the materials sorted by type of use. <br> 
 	* <b> pre: </b>  the enumeration should have been assigned before. <br>
-   	* <b> post: the method will show the places with the lower prices of each material and a better budget in that case.
-	* @param black_work[] this array will save the materials that will be used for black work.
-	* @param white_work[] this array will save the materials that will be used for white work.
-	* @param painting[]   this array will save the materials that will be used for painting.
-	* @param kind[]       this contains the enumaration of the type of work. 
-	* @param materials_list[] this contains the list of materials. 
+   	* <b> post:</b> the method will show the places with the lower prices of each material and a better budget in that case.<br>
+	* @param kind ,this contains the enumaration of the type of work. 
+	* @param materials_list this contains the list of materials. 
 	* @return 
 	*/
 	
-	public static void type_ofuse(String black_work[], String white_work[], String painting[], Work kind[], String materials_list[]){
+	public static void type_ofuse(Work kind[], String materials_list[]){
 		int election_work;
 		String final_response;
 		sc.nextLine();
-		System.out.println("Would you like to see a list of the materials sorted by the type of work? type yes or no:");
+		System.out.println();
+		System.out.print("Would you like to see a list of the materials sorted by the type of work? type yes or no:");
 		final_response=sc.nextLine();
 		while(final_response.equals("yes")){
 			
@@ -248,41 +320,26 @@ public class Proyecto_integrador1{
 			case 1: 
 				   for(int i=0; i<materials_list.length; i++){
 						 if(kind[i]==Work.OBRA_NEGRA){
-                               black_work[i]=materials_list[i];						 
+                               System.out.println(materials_list[i]);						 
 						         }
 					        }
-                   System.out.println("the list of materials that will be used for Black work are");				
-				   for(int j=0; j<materials_list.length; j++){
-					   if(black_work[j]!=null){
-						   System.out.println(black_work[j]);
-					         }
-				          }
+				
 				       break;
 			case 2:
 				  for(int i=0; i<materials_list.length; i++){
 						     if(kind[i]==Work.OBRA_BLANCA){
-                                  	white_work[i]=materials_list[i];						 
+                                  	System.out.println(materials_list[i]);						 
 							         }
 					        }
-				System.out.println("the list of materials that will be used for White work  are");
-                  for(int j=0; j<materials_list.length; j++){
-					   if(white_work[j]!=null){
-						   System.out.println(white_work[j]);
-					         }
-				          }				
+				
+			
 				      break;
 			case 3:
 				  for(int i=0; i<materials_list.length; i++){
 						     if(kind[i]==Work.PINTURA){
-                                  	painting[i]=materials_list[i];						 
+                              System.out.println(materials_list[i]);						 
 							      }
 					        }
-				  System.out.println("the list of materials that will be used for Painting  are");
-				  for(int j=0; j<materials_list.length; j++){
-					   if(painting[j]!=null){
-						   System.out.println(painting[j]);  
-					         }
-				          }
 				        break;
 		   }
 		sc.nextLine();
